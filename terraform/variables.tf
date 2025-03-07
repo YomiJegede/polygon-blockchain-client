c2a2245c20a0d1e919800eacf7ac31e37bf7643a
YomiJegede <39343831+YomiJegede@users.noreply.github.com>
2025-03-07 16:05:37 +0000

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ecs_cluster_name" {
+  default = "blockchain-cluster"
+}
+
+variable "ecs_task_family" {
+  default = "blockchain-task"
+}
